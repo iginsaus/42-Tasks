0e73a52cb5c5aeaad7f9b67c14a93dd1b63992b9
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-10 00:09:18 +0100

    Add files via upload

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -6,20 +6,27 @@
 /*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 08:35:48 by iginsaus          #+#    #+#             */
-/*   Updated: 2023/12/19 17:11:48 by iginsaus         ###   ########.fr       */
+/*   Updated: 2023/12/14 09:08:53 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int c)
+int   ft_isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-	{	
-		return (0);
-	}
+   return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+   char c;
+
+   printf("Input an alphabetical character to obtain 1: ");
+   scanf("%c", &c);
+
+   printf("%d", ft_isalpha(c));
+   return (0);
+}
+*/
