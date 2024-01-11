c59c3e1b81034cba6ee3f9e6f88cef60106b3f6c
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-11 12:49:00 +0100

    Update ft_strlcpy.c

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -14,19 +14,23 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
+	unsigned int	i;
+	unsigned int	c;
 
 	i = 0;
-	if (dstsize == 0)
-		return (ft_strlen(src));
-	while (src[i] != '\0' && i < (dstsize - 1))
-	{
-		dst[i] = src[i];
-		i++;
-	}
+	c = 0;
+	while (src[c] != '\0')
+		c++;
 	if (dstsize != 0)
+	{
+		while (src[i] != '\0' && i < (dstsize - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
 		dst[i] = '\0';
-	return (ft_strlen(src));
+	}
+	return (c);
 }
 
 #include <stdio.h>
