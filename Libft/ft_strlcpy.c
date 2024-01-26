f1fa88bfc114589dd952e83127d5d78695d68258
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-26 16:59:08 +0100

    Update ft_strlcpy.c

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -35,15 +35,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 /*
 #include <stdio.h>
 
-int main() {
-  char src[] = "Hola, mundo!";
-  char dst[4];
+int	main(void)
+{
+	char	src[10] = "Bye World!";
+	char	dst[4];
+	size_t	len = ft_strlcpy(dst, src, sizeof(dst));
 
-  size_t len = ft_strlcpy(dst, src, sizeof(dst));
-
-  printf("La cadena copiada es: %s\n", dst);
-  printf("La longitud de la cadena original es: %zu\n", len);
-
-  return 0;
+	printf("La cadena copiada es: %s\n", dst);
+	printf("La longitud de la cadena original es: %zu\n", len);
+	return (0);
 }
 */
