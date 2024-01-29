9e228c9addb2eb1f6c80981b12c0e75feb881514
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-29 18:01:47 +0100

    Update ft_putstr_fd.c

--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_putstr_fd(char *s, int fd) {
+    if (!s) {
+        return;
+    }
+
+    write(fd, s, ft_strlen(s));
+}
+/*
+int main() {
+    ft_putstr_fd("Hola, mundo", 1);
+    return 0;
+}
+*/
 
