b2cea49a551a20354991cbe56865457ffbc58b2c
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-29 18:03:45 +0100

    Update ft_putnbr_fd.c

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd) 
+{
+    char c;
+
+    if (n == -2147483648) 
+    {
+        write(fd, "-2147483648", ft_strlen("-2147483648"));
+        return;
+    }
+
+    if (n < 0) 
+    {
+        write(fd, "-", 1);
+        n = -n;
+    }
+
+    while (n > 0) 
+    {
+        c = n % 10 + '0';
+        write(fd, &c, 1);
+        n /= 10;
+    }
+}
+/*
+int main(void)
+{
+	int fd=1;
+	int n =-2147483648;
+	ft_putnbr_fd(n,fd);
+}
+*/
 
