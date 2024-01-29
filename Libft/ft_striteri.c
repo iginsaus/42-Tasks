3feb7aadaf1348b586d6604e3cfb61814006651b
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-29 18:00:25 +0100

    Update ft_striteri.c

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_striteri(char *s, void (*f)(unsigned int, char *)) {
+  unsigned int i = 0;
+
+  if (!s || !f) {
+    return;
+  }
+
+  while (s[i]) {
+    f(i, &s[i]);
+    i++;
+  }
+}
+/*
+void custom_function(unsigned int i, char *c) {
+  (void)i; // To suppress the unused parameter warning
+  *c = ft_toupper((int)*c);
+}
+
+int main() {
+  char s[] = "Hola, mundo";
+
+  ft_striteri(s, &custom_function);
+
+  printf("String: %s\n", s);
+
+  return 0;
+}
+*/
 
