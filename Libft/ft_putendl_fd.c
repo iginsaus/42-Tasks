4c2b2f443927291143e3c6f25bb078f8fd258b8b
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-29 18:02:42 +0100

    Update ft_putendl_fd.c

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_putendl_fd(char *s, int fd) {
+  if (!s) {
+    return;
+  }
+
+  write(fd, s, ft_strlen(s));
+  write(fd, "\n", 1);
+}
+/*
+int main (void)
+{
+	ft_putendl_fd("Adios Mundo Cruel", 1);
+	return (0);
+}
+*/
 
