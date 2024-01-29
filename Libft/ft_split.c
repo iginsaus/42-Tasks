c5d068972a384a25ed0cebdc315050bb28f93069
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-29 17:54:52 +0100

    Update ft_split.c

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -10,4 +10,79 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char **ft_split(char const *s, char c) {
+  char **arr;
+  size_t i, start, len;
+
+  if (!s) {
+    return NULL;
+  }
+
+  i = 0;
+  while (s[i] == c) {
+    i++;
+  }
+
+  if (s[i] == '\0') {
+    return (char **)malloc(sizeof(char *) * 1);
+  }
+
+  start = i;
+  len = 1;
+
+  while (s[i]) {
+    if (s[i] == c) {
+      arr = (char **)realloc(arr, sizeof(char *) * (len + 1));
+      if (!arr) {
+        return NULL;
+      }
+
+      arr[len - 1] = ft_substr(s, start, i - start);
+      if (!arr[len - 1]) {
+        return NULL;
+      }
+
+      start = i + 1;
+      len++;
+    }
+    i++;
+  }
+
+  arr = (char **)realloc(arr, sizeof(char *) * (len + 1));
+  if (!arr) {
+    return NULL;
+  }
+
+  arr[len - 1] = ft_substr(s, start, i - start);
+  if (!arr[len - 1]) {
+    return NULL;
+  }
+
+  return arr;
+}
+/*
+int main() {
+  char *s = "Hola, mundo, hola";
+  char **arr;
+  int i;
+
+  arr = ft_split(s, ',');
+  if (arr == NULL) {
+    printf("Error allocating memory for array\n");
+    return 1;
+  }
+
+  for (i = 0; arr[i]; i++) {
+    printf("Subcadena %d: %s\n", i, arr[i]);
+  }
+
+  for (i = 0; arr[i]; i++) {
+    free(arr[i]);
+  }
+  free(arr);
+
+  return 0;
+}
+*/
