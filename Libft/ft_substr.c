1e6eeec1aeb8e1d9b57cf59c1a6b7fdc02790bf9
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-29 17:48:49 +0100

    Update ft_substr.c

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -10,4 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+  if (!s) 
+  {
+    return (NULL);
+  }
+
+  if (start >= ft_strlen(s)) 
+  {
+    return ft_strdup("");
+  }
+
+  if (len > ft_strlen(s) - start) 
+  {
+    len = ft_strlen(s) - start;
+  }
+
+  char *substr = malloc(len + 1);
+  if (!substr) 
+  {
+    return (NULL);
+  }
+
+  strncpy(substr, s + start, len);
+  substr[len] = '\0';
+
+  return (substr);
+}
+/*
+char *ft_substr(char const *s, unsigned int start, size_t len);
+
+int main() {
+  char s[] = "Hello, World!";
+  char *substring;
+
+  substring = ft_substr(s, 7, 6);
+  if (substring) {
+    printf("Substring: %s\n", substring);
+    free(substring);
+  } else {
+    printf("Error allocating memory for substring\n");
+  }
+
+  return 0;
+}
+*/
