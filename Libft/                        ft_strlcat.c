ab42a7d396e1d07ff6c5862aed1167c6ed471bd6
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-30 17:23:50 +0100

    Update                         ft_strlcat.c

--- Libft/                        ft_strlcat.c
+++ Libft/                        ft_strlcat.c
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: iginsaus <iginsaus@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 14:06:40 by iginsaus          #+#    #+#             */
-/*   Updated: 2024/01/11 14:14:53 by iginsaus         ###   ########.fr       */
+/*   Created: 2024/01/30 17:22:48 by iginsaus          #+#    #+#             */
+/*   Updated: 2024/01/30 17:22:52 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	l;
 
 	l = ft_strlen(dst) + ft_strlen(src);
-	if (size <= ft_strlen(dst))
-		return (ft_strlen(src) + size);
+	if (dstsize <= ft_strlen(dst))
+		return (ft_strlen(src) + dstsize);
 	while (*dst)
 		dst++;
 	i = 0;
-	while ((i < size - (l - ft_strlen(src)) - 1) && src[i])
+	while ((i < dstsize - (l - ft_strlen(src)) - 1) && src[i])
 	{
 		dst[i] = src[i];
 		i++;
@@ -31,3 +31,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst[i] = '\0';
 	return (l);
 }
+/*
+int	main(void)
+{
+	char	src[20] = " friend";
+	char	dst[20] = "Hello";
+
+	printf("%zu\nsrc: %s\ndst: %s\n", ft_strlcat(dst, src, 13), src, dst);
+}
+*/
