d6dce47c7736b4a4e48c9fde05bde26bd912cd68
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-31 18:13:56 +0100

    Update            ft_substr.c

--- Libft/           ft_substr.c
+++ Libft/           ft_substr.c
@@ -3,58 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: iginsaus <iginsaus@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/24 17:23:10 by iginsaus          #+#    #+#             */
-/*   Updated: 2024/01/24 17:23:32 by iginsaus         ###   ########.fr       */
+/*   Created: 2024/01/31 17:22:56 by iginsaus          #+#    #+#             */
+/*   Updated: 2024/01/31 17:53:42 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-  if (!s) 
-  {
-    return (NULL);
-  }
+	char	*substr;
+	size_t	i;
 
-  if (start >= ft_strlen(s)) 
-  {
-    return ft_strdup("");
-  }
-
-  if (len > ft_strlen(s) - start) 
-  {
-    len = ft_strlen(s) - start;
-  }
-
-  char *substr = malloc(len + 1);
-  if (!substr) 
-  {
-    return (NULL);
-  }
-
-  strncpy(substr, s + start, len);
-  substr[len] = '\0';
-
-  return (substr);
+	if (!s)
+		return (NULL);
+	if (len == 0 || ft_strlen(s) == 0 || ft_strlen(s) <= start)
+		return (ft_strdup(""));
+	i = 0;
+	if (ft_strlen(s) - start > len)
+		i = len + 1;
+	else
+		i = ft_strlen(s) - start + 1;
+	substr = ft_calloc(sizeof(char), i);
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, s + start, i);
+	return (substr);
 }
 /*
-char *ft_substr(char const *s, unsigned int start, size_t len);
+int	main(void)
+{
+	char	s[] = "Hello, World!";
+	char	*substring;
 
-int main() {
-  char s[] = "Hello, World!";
-  char *substring;
-
-  substring = ft_substr(s, 7, 6);
-  if (substring) {
-    printf("Substring: %s\n", substring);
-    free(substring);
-  } else {
-    printf("Error allocating memory for substring\n");
-  }
-
-  return 0;
+	substring = ft_substr(s, 7, 6);
+	if (substring)
+	{
+		printf("Substring: %s\n", substring);
+		free (substring);
+	}
+	else
+	{
+		printf("Error allocating memory for substring\n");
+	}
+	return (0);
 }
 */
