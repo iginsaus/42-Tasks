de50ee148e16315a400cfac7b61bc05df0608d28
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-31 18:12:53 +0100

    Update             ft_strdup.c

--- Libft/            ft_strdup.c
+++ Libft/            ft_strdup.c
@@ -3,37 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: iginsaus <iginsaus@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 17:32:39 by iginsaus          #+#    #+#             */
-/*   Updated: 2024/01/11 17:32:44 by iginsaus         ###   ########.fr       */
+/*   Created: 2024/01/31 17:10:45 by iginsaus          #+#    #+#             */
+/*   Updated: 2024/01/31 17:21:26 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strdup(const char *src)
+char	*ft_strdup(const char *src)
 {
-    char *dest;
-    size_t len;
+	char	*dest;
+	size_t	len;
 
-    len = ft_strlen(src);
-    dest = (char *)malloc(len + 1);
-    if (dest == NULL)
-        return (NULL);
-    ft_memcpy(dest, src, len + 1);
-    return (dest);
+	len = ft_strlen(src);
+	dest = (char *)malloc(len + 1);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	ft_memcpy(dest, src, len + 1);
+	return (dest);
 }
 /*
-int main()
+int	main(void)
 {
-    char *str1 = "Hola, mundo";
-    char *str2;
+	char	*str1 = "Hello my friend";
+	char	*str2;
 
-    str2 = ft_strdup(str1);
-    printf("%s\n", str2);
-    free(str2);
-    return (0);
+	str2 = ft_strdup(str1);
+	printf("%s\n", str2);
+	free (str2);
+	return (0);
 }
 */
-
