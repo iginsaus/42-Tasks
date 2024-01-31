ba5eaec193e9181fbec1f648ffef981c5b053244
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-01-31 18:10:22 +0100

    Update                ft_strnstr.c

--- Libft/               ft_strnstr.c
+++ Libft/               ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: iginsaus <iginsaus@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 17:30:54 by iginsaus          #+#    #+#             */
-/*   Updated: 2024/01/23 18:40:24 by iginsaus         ###   ########.fr       */
+/*   Created: 2024/01/31 14:04:07 by iginsaus          #+#    #+#             */
+/*   Updated: 2024/01/31 16:18:26 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
+	if (needle[0] == '\0')
+		return ((char *) haystack);
 	while (i < len && haystack[i])
 	{
 		j = 0;
@@ -27,36 +29,32 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		}
 		if (needle[j] == '\0')
 		{
-			return ((char *)haystack + i);
+			return ((char *)&haystack[i]);
 		}
 		i++;
 	}
 	return (NULL);
 }
 /*
-#include <string.h>
-#include <stdio.h>
-
-int main() 
+int	main(void)
 {
-  char haystack[9] = "Nora zoaz";
-  char needle[3] = "oaz";
+	char	haystack[] = "Nora zoaz zu";
+	char	needle[] = "oaz";
 
-  char *result = ft_strnstr(haystack, needle, strlen(haystack));
-
-  if (result) 
-  {
-    printf("ft_strnstr() has found '%s' in string '%s'.\n", 
-	needle, haystack);
-    printf("substring poisition is  %lu in the string.\n", 
-	(result - haystack)+1);
-  } 
-  else 
-  {
-    printf("ft_strnstr() unable to find '%s' in string '%s'.\n", 
-	needle, haystack);
-  }
-
-  return (0);
+	char	*result = ft_strnstr(haystack, needle, strlen(haystack));
+	if (result)
+	{
+		printf("ft_strnstr() has found '%s' in string '%s'.\n",
+			needle, haystack);
+		printf("substring poisition is  %lu in the string.\n",
+			(result - haystack) + 1);
+		printf("result is %s", result);
+	}
+	else
+	{
+		printf("ft_strnstr() unable to find '%s' in string '%s'.\n",
+			needle, haystack);
+	}
+	return (0);
 }
 */
