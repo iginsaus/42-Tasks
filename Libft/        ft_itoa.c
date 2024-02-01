61743c243a5ca03c2437579451644db190fba82e
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-02-01 15:07:05 +0100

    Update         ft_itoa.c

--- Libft/        ft_itoa.c
+++ Libft/        ft_itoa.c
@@ -3,11 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: iginsaus <iginsaus@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/24 17:25:53 by iginsaus          #+#    #+#             */
-/*   Updated: 2024/01/24 17:25:58 by iginsaus         ###   ########.fr       */
+/*   Created: 2024/02/01 14:57:18 by iginsaus          #+#    #+#             */
+/*   Updated: 2024/02/01 15:05:24 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+static int	ft_negative(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+static int	ft_nlen(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		len++;
+		n = n / 10;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		n_leng;
+
+	n_leng = ft_nlen(n);
+	str = malloc(sizeof(char) * n_leng + 1);
+	if (!str)
+		return (NULL);
+	str[n_leng] = '\0';
+	if (n < 0)
+		str[0] = '-';
+	if (n == 0)
+		str[0] = '0';
+	while (n != 0)
+	{
+		n_leng --;
+		str[n_leng] = ft_negative(n % 10) + '0';
+		n = n / 10;
+	}
+	return (str);
+}
+/*
+int	main(void)
+{
+	int	s;
+
+	s = 42;
+	printf("Solution to evthg is: %s\n", ft_itoa (s));
+}
+*/
