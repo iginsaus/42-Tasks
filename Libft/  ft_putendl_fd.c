fbccb9fbd570b262b1d749927061ecfdfa5bf6b9
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-02-01 14:05:25 +0100

    Update   ft_putendl_fd.c

--- Libft/  ft_putendl_fd.c
+++ Libft/  ft_putendl_fd.c
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: iginsaus <iginsaus@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/24 17:28:10 by iginsaus          #+#    #+#             */
-/*   Updated: 2024/01/24 17:28:15 by iginsaus         ###   ########.fr       */
+/*   Created: 2024/02/01 13:37:11 by iginsaus          #+#    #+#             */
+/*   Updated: 2024/02/01 13:40:15 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd) {
-  if (!s) {
-    return;
-  }
-
-  write(fd, s, ft_strlen(s));
-  write(fd, "\n", 1);
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (!s)
+	{
+		return ;
+	}
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
 }
 /*
-int main (void)
+int	main(void)
 {
 	ft_putendl_fd("Adios Mundo Cruel", 1);
 	return (0);
 }
 */
-
