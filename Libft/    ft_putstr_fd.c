c13df214ac67254f0855ce9b7e2e4ab3a0a56e07
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-02-01 14:04:26 +0100

    Update     ft_putstr_fd.c

--- Libft/    ft_putstr_fd.c
+++ Libft/    ft_putstr_fd.c
@@ -3,26 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: iginsaus <iginsaus@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/24 17:27:47 by iginsaus          #+#    #+#             */
-/*   Updated: 2024/01/24 17:27:49 by iginsaus         ###   ########.fr       */
+/*   Created: 2024/02/01 13:58:26 by iginsaus          #+#    #+#             */
+/*   Updated: 2024/02/01 14:02:55 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putstr_fd(char *s, int fd) {
-    if (!s) {
-        return;
-    }
-
-    write(fd, s, ft_strlen(s));
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+	{
+		return ;
+	}
+	write(fd, s, ft_strlen(s));
 }
 /*
-int main() {
-    ft_putstr_fd("Hola, mundo", 1);
-    return 0;
+int	main(void)
+{
+	ft_putstr_fd("Hello my friend\n", 1);
+	return (0);
 }
 */
-
