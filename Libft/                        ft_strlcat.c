aaffc0ba7a359722d882b1db8f9d4db863f98adc
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-02-17 12:19:43 +0100

    Update                         ft_strlcat.c

--- Libft/                        ft_strlcat.c
+++ Libft/                        ft_strlcat.c
@@ -12,31 +12,35 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	l;
+	size_t	j;
 
-	l = ft_strlen(dst) + ft_strlen(src);
-	if (dstsize <= ft_strlen(dst))
-		return (ft_strlen(src) + dstsize);
+	j = ft_strlen(dst) + ft_strlen(src);
+	if (size <= ft_strlen(dst))
+		return (ft_strlen(src) + size);
 	while (*dst)
 		dst++;
 	i = 0;
-	while ((i < dstsize - (l - ft_strlen(src)) - 1) && src[i])
+	while ((i < size - (j - ft_strlen(src)) - 1) && src[i])
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	return (l);
+	return (j);
 }
 /*
-int	main(void)
+int main(void)
 {
-	char	src[20] = " friend";
-	char	dst[20] = "Hello";
+    char src[20] = " friend";
+    char dst[20] = "Hello";
 
-	printf("%zu\nsrc: %s\ndst: %s\n", ft_strlcat(dst, src, 13), src, dst);
+		printf("Source: %s\n", src);
+		printf("Destination: %s\n", dst);
+		printf("Length of string concatenated: %zu\n", ft_strlcat(dst, src, 20));
+    printf("Concatenated dst: %s\n", dst);
+    return (0);
 }
 */
