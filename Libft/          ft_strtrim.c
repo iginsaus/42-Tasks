2ab2bbf73289cc783f85fd9f8c13b976cd74ded9
iginsaus <153928650+iginsaus@users.noreply.github.com>
2024-02-19 20:11:56 +0100

    Update           ft_strtrim.c

--- Libft/          ft_strtrim.c
+++ Libft/          ft_strtrim.c
@@ -17,7 +17,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	len;
 
 	if (!s1 || !set)
-		return (0);
+		return (NULL);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	len = ft_strlen(s1);
